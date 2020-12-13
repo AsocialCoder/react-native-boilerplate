10b2b45cba9e9ba3f03eb23dc357b9bef5e5511e
Ahmet Emre ÇETİN <cetinahmetemre@hotmail.com>
2020-12-13 22:31:33 +0300

    feat: add react-native-vector-icons

--- babel.config.js
+++ babel.config.js
@@ -1,23 +1,22 @@
 module.exports = {
-  presets: ["module:metro-react-native-babel-preset"],
-  "plugins": [
-      [
-          "module-resolver",
-          {
-              root: ["."],
-              alias: {
-                  "components": "./src/components",
-                  "config": "./src/config",
-                  "consts": "./src/consts",
-                  "models": "./src/models",
-                  "screens": "./src/screens",
-                  "utils": "./src/utils",
-                  "navigation": "./src/navigation",
-                  "reducers": "./src/reducers",
-                  "actions": "./src/actions",
-              },
-          },
-      ],
-  ],
-  
+    presets: ["module:metro-react-native-babel-preset"],
+    plugins: [
+        [
+            "module-resolver",
+            {
+                root: ["."],
+                alias: {
+                    components: "./src/components",
+                    config: "./src/config",
+                    consts: "./src/consts",
+                    models: "./src/models",
+                    screens: "./src/screens",
+                    utils: "./src/utils",
+                    navigation: "./src/navigation",
+                    reducers: "./src/reducers",
+                    actions: "./src/actions",
+                },
+            },
+        ],
+    ],
 };
