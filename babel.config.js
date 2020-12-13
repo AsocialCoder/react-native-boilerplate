ad38039c1c61810604bc2c895905c44f8a8aff7a
Ahmet Emre ÇETİN <cetinahmetemre@hotmail.com>
2020-12-13 22:18:32 +0300

    feat: add babel module resolver for cleaning up imports

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,23 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+  presets: ["module:metro-react-native-babel-preset"],
+  "plugins": [
+      [
+          "module-resolver",
+          {
+              root: ["."],
+              alias: {
+                  "components": "./src/components",
+                  "config": "./src/config",
+                  "consts": "./src/consts",
+                  "models": "./src/models",
+                  "screens": "./src/screens",
+                  "utils": "./src/utils",
+                  "navigation": "./src/navigation",
+                  "reducers": "./src/reducers",
+                  "actions": "./src/actions",
+              },
+          },
+      ],
+  ],
+  
 };
